92c3f3d2116c708d290298baecd63be9eb05307c
Paul Summers <paulsumm@microsoft.com>
2018-03-07 10:39:13 -0500

    Add zip parameter for places service

--- app/workfrom/places.js
+++ app/workfrom/places.js
@@ -1,18 +1,34 @@
 const request = require('request-promise');
 
-function getPlacesByZipCode(zipcode) {
-    request({
-        uri: 'http://api.workfrom.co/places/postal/01803',
-        qs: {
-            appid: 'p9Q03LK3GbnwVQGL'
-        },
-        json: true
-    }).
-    then((data) => {
+var baseUri = 'http://api.workfrom.co/places/';
+
+function getPlacesByZipCode(zipcode, appid, dow) {
+    var qs =  {
+        appid: appid
+    }
+    if(dow){
+        qs.dow = dow;
+    }
+    var requestPath = 'postal/' + zipcode;
+    getPlaces(requestPath, qs).then((data) => {
         console.log(data);
     }).catch((err) => {
         console.log(err);
     });
+}
+
+function getPlaces(endpoint, qs){
+    var requestUri = baseUri + endpoint;
+    console.log(requestUri);
+    return request({
+        uri: requestUri,
+        qs: qs,
+        json: true
+    }).then ((data) => {
+        return data;
+    }).catch((err) => {
+        throw err;
+    })
 }
 
 module.exports = {
