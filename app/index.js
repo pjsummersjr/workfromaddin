df9d14dbfde6b2a6b6487cfd57e8a278db36e3e3
Paul Summers <paulsumm@microsoft.com>
2018-03-07 10:39:37 -0500

    Add zip parameter for places service - index.js

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ const hbs = require('express-handlebars');
 const places = require('./workfrom/places');
 const app = express();
 const port = 3000;
+const workFromAppId = 'p9Q03LK3GbnwVQGL';
 
 app.engine('.hbs', hbs({
     defaultLayout: 'main',
@@ -14,8 +15,8 @@ app.engine('.hbs', hbs({
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'web/views'));
 
-app.get('/', (request, response) => {
-    places.getPlacesByZipCode("01803");
+app.get('/:zip', (request, response) => {
+    places.getPlacesByZipCode(request.params.zip, workFromAppId);
     response.render('home', {
         name: 'Paul Summers'
     });
