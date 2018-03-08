8aff8f1c190d787950d3ab8b88d10087f3cb8053
Paul Summers <paulsumm@microsoft.com>
2018-03-07 21:31:08 -0500

    Added bunyan logging

--- app/index.js
+++ app/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const hbs = require('express-handlebars');
 const places = require('./workfrom/places');
 const apiRouter = require('./workfrom/apiRouting');
+
+const Logger = require('bunyan');
+const log = new Logger({name:'server-log'});
+
 const app = express();
 const port = 3000;
 const workFromAppId = 'p9Q03LK3GbnwVQGL';
@@ -25,22 +29,13 @@ app.get('/', (request, response) => {
     })
 });
 
-/* app.get('/api/:zip', (request, response) => {
-    places.getPlacesByZipCode(request.params.zip, workFromAppId)
-    .then((data) => {
-        response.send(data);
-    })
-    .catch((err) => {
-        response.send(err);
-    });
-}); */
 
 app.listen(port, (err) => {
     if(err){
-        return console.log("Error starting application" , err);
+        return log.error("Error starting application" , err);
     }
 
-    console.log("Express server running on port ${port}");
+    log.info("Express server running on port ${port}");
 })
 
 module.exports.app = app;
